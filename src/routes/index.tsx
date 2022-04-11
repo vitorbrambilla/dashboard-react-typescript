c123293ede6638957c9df749d6057d538703497f
vitorbrambilla <vitorbrambilla10@gmail.com>
2022-04-11 17:15:26 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+import App from './app.routes';
+import AuthRoutes from './auth.routes';
+
+const Routes: React.FC = () => {
+  const { logged } = useAuth();
+
+  return (
+  <BrowserRouter>
+    {logged ? <App/> : <AuthRoutes />}
+  </BrowserRouter>
+)};
+
+export default Routes;
